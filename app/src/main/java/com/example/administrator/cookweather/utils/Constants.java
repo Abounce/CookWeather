faeabf89e122fed916c43f9e2ee24a78c68f4567
Abounce <784354821@qq.com>
2017-02-20 15:49:30 +0800

    first
    first

---
+++ app/src/main/java/com/example/administrator/cookweather/utils/Constants.java
@@ -0,0 +1,14 @@
+package com.example.administrator.cookweather.utils;
+
+/**
+ * @author yhy created at 2017/2/20 15:24
+ */
+
+public class Constants {
+    //省份列表
+    public static final String baseProvince="http://guolin.tech/api/china";
+    //和风天气的key
+    public static final String API_KEY="854043d5ba7f4e16a16f353f70bd2f78";
+
+    String a="http://guolin.tech/api/weather?cityid=CN101190401&key=854043d5ba7f4e16a16f353f70bd2f78";
+}
